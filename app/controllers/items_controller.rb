2fd17d53d5f2c9b9d7b28d403511533cc3da3ce4
dahuizhangtian7-bit <dahuizhangtian7@gmail.com>
2026-05-07 14:47:02 +0900

    商品出品情報の登録

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,4 +8,19 @@ class ItemsController < ApplicationController
     @item = Item.new
   end
 
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image, :name, :content, :category_id, :status_id, :cost_id, :area_id, :day_id, :price).merge(user_id: current_user.id)
+  end
+
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,27 @@
 class Item < ApplicationRecord
+  validates :image, presence: true
   validates :name, presence: true
   validates :content, presence: true
-  validates :price, presence: true
+  validates :price, presence: true, numericality: {
+    only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999
+  }, format: {
+    with: /\A[0-9]+\z/
+  }
 
   belongs_to :user
+  has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :cost
+  belongs_to :area
+  belongs_to :day
+
+  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :status_id, numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :cost_id, numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :area_id, numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :day_id, numericality: { other_than: 1 , message: "can't be blank"} 
+
 end
