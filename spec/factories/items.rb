36fb36c8454f892e822494f2b500a32d3e8466fd
dahuizhangtian7-bit <dahuizhangtian7@gmail.com>
2026-05-07 17:08:33 +0900

    itemモデルのテスト

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,18 @@
 FactoryBot.define do
   factory :item do
+    name        { 'test' }
+    content     { 'testの説明' }
+    category_id { 2 }
+    status_id   { 2 }
+    cost_id     { 2 }
+    area_id     { 2 }
+    day_id      { 2 }
+    price       { 1000 }
     
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
