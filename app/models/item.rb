81f2f89f08a3171b265cd9abd0b7a6adc50e4108
dahuizhangtian7-bit <dahuizhangtian7@gmail.com>
2026-05-08 11:28:43 +0900

    コードの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,8 +4,6 @@ class Item < ApplicationRecord
   validates :content, presence: true
   validates :price, presence: true, numericality: {
     only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999
-  }, format: {
-    with: /\A[0-9]+\z/
   }
 
   belongs_to :user
