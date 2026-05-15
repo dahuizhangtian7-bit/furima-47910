c02f687772bf8d5d30f673c8153f4c7b36624145
dahuizhangtian7-bit <dahuizhangtian7@gmail.com>
2026-05-15 11:36:25 +0900

    PAY.JPの実装

--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -1,6 +1,6 @@
 class PurchaseAddress
   include ActiveModel::Model
-  attr_accessor :user_id, :item_id, :postal, :area_id, :city, :street, :building, :phone_number
+  attr_accessor :user_id, :item_id, :postal, :area_id, :city, :street, :building, :phone_number, :token
 
   with_options presence: true do
     validates :user_id
@@ -9,6 +9,7 @@ class PurchaseAddress
     validates :city
     validates :street
     validates :phone_number, format: {with: /\A[0-9]{10,11}\z/, message: "is invalid"}
+    validates :token
   end
   validates :area_id, numericality: { other_than: 1, message: "can't be blank" }
 
